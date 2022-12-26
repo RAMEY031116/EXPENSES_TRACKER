b4406dd52e9f4d9a7b4355318cb0508bcba4040c
ramey <ramey@ramey.ramey>
2022-12-26 21:59:20 +0000

    have added few feature on the app

--- main.py
+++ main.py
@@ -18,6 +18,16 @@ def clear():
   os.system("clear")
   return header()
 
+def search():
+  date = input(f"Enter your transaction date  YYYY-MM-DD : ")
+  
+  search_file = csv.reader(open("expenses.csv","r"))
+
+  for row in search_file:
+    if date == row[0]:
+      print(row)
+
+
 
 
 def add_expenses():
@@ -28,25 +38,28 @@ def add_expenses():
   travel = []
   amount = []
 
+
   name = input (f"Please enter your name : ")
   shop_name = input(f"Please enter a shop name : ")
   food = float(input (f"Please enter an amount: "))
   car = float(input (f"please enter an ammount for Petrol : "))
   total_amount = food+car
   
+
   groceries.append(food)
   travel.append(car)
   shop.append(shop_name)
   amount.append(total_amount)
   names.append(name)
 
-  print (f"The Total Ammount you have spent on {date.today()} is {total_amount}  \n")
+  print (f"The Total Ammount you have spent on {today()} is {str(total_amount)}\n")
 
 
   with open("expenses.csv", "a")as f:
     writer = csv.writer(f)
-
+    
     writer.writerow([today(),names,shop,groceries,travel,amount])
+    
     
 
 
@@ -63,17 +76,30 @@ header()
 print(f"Please choose from the following :)" "\n")
 
 while True:
-  mode = input ("[1] If you would like to add your transaction to excel then press 1 \n"
+  mode_first = input(f"[0] If you would like to search then press 0 \n"
+  "[1]If you would like to add your transaction then press 1 \n"
+  "[2] If you woule like to view your transaction then press 2 \n"
+  "Enter your mode: ").lower()
+  if mode_first == "0":
+    search()
+  elif mode_first == "1":
+    add_expenses()
+  elif mode_first == "2":
+    view()
+  else:
+    continue
+
+  mode_second= input ("\n""[1] If you would like to save your transaction then press 1 \n"
   "[2] if you would like to view your transaction then press 2 \n"
   "[3] If you would like to quit press 3 \n" 
   "Enter Your mode : ").lower()
 
-  if mode == "3":
+  if mode_second == "3":
     print(f"HAVE A BEAUTIFUL DAY !!")
     exit()
-  elif mode == "1":
+  elif mode_second == "1":
     add_expenses()
-  elif mode == "2":
+  elif mode_second== "2":
     view()
   else :
     continue
